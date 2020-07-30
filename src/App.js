4da654b7726d32521212ff5bb1a0ee0ed7f45668
DoanLinhTrang <67568474+DoanLinhTrang@users.noreply.github.com>
2020-07-30 08:27:45 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,6 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Header from './components/Header'
-import Footer from './components/Footer';
-import TrainingHung from './components/trainingHung'
-import Example from './components/example';
-import PopUp from "./components/popup"
-import MaterialTable from 'material-table';
-import MaterialTableDemo from './components/container';
-import AddProduct from './components/AddProduct';
 import List from './components/list';
 
 // Styles
