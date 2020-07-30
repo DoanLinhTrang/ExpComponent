cecee9d4152d66b99431bb886d9d42178b8e4283
DoanLinhTrang <67568474+DoanLinhTrang@users.noreply.github.com>
2020-07-30 08:27:17 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -16,31 +16,8 @@ import List from './components/list';
 function App() {
   return (
     <div className="App">
-
-      {/* <Header /> */}
-      {/* <PopUp/> */}
-      {/* <AddProduct/> */}
-      {/* <Footer />
-      <Example /> */}
-      {/* <MaterialTableDemo /> */}
       <List />
       
-
-      {/* <TrainingHung /> */}
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
     </div>
   );
 }
